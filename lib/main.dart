3633f2d997e44f7260291ec1e07332d8d418dac4
kimjihye058 <s2321@e-mirim.hs.kr>
2025-06-11 03:25:29 +0900

    [feat] : signup 화면 구현 및 백엔드 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:journimal_client/screen/register/signup.dart';
+import 'package:journimal_client/screen/%08auth/signup.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
