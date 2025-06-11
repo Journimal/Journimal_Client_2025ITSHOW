57d0c7a1b05f578dbd8765ebb8800d1fdb9b4b36
kimjihye058 <s2321@e-mirim.hs.kr>
2025-06-11 15:41:54 +0900

    [feat] : register_date 화면 구현 및 백엔드 연결

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -1,23 +1,17 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-// final TokenService tokenService = TokenService();
-// String? token = await tokenService.getToken('authToken');
-
 class TokenService {
-  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  // save Token method
   Future<void> saveToken(String token) async {
-    await secureStorage.write(key: 'authToken', value: token);
+    await _storage.write(key: 'accessToken', value: token);
   }
 
-  // get Token method
-  Future<String?> getToken(String key) async {
-    return await secureStorage.read(key: 'authToken');
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'accessToken');
   }
 
-  // delete Token method
-  Future<void> deleteToken(String key) async {
-    await secureStorage.delete(key: 'authToken');
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'accessToken');
   }
 }
